7786350e74cb1ae913a2dd6dad150190dd544ece
George Maximenko <30684663+mvximenko@users.noreply.github.com>
2024-02-24 22:05:17 +0300

    Update styles

--- src/App.jsx
+++ src/App.jsx
@@ -22,16 +22,36 @@ const App = () => {
 
   return (
     <>
-      <div style={{ margin: '0 5%' }}>
-        <input type='file' onChange={handleChange} />
-        <h1>Unique Words: {state.length}</h1>
+      <header className='h-14 shadow bg-white flex items-center font-bold text-2xl '>
+        <div className='container mx-auto px-4 md:px-24'>Wordy</div>
+      </header>
 
-        {!!state.length &&
-          state.map(([word, frequency]) => (
-            <p key={word}>
-              <strong>{word}</strong> - {frequency}
-            </p>
-          ))}
+      <div className='container mx-auto px-4 md:px-24 mt-6'>
+        <label className='mb-2 text-sm font-medium' htmlFor='file'>
+          Choose .epub, .txt, .srt file
+        </label>
+
+        <input
+          type='file'
+          id='file'
+          className='block w-full h-10 border border-gray-200 shadow cursor-pointer file:border-0 file:h-full file:cursor-pointer'
+          accept='.epub,.txt,.srt'
+          onChange={handleChange}
+        />
+
+        {!!state.length && (
+          <>
+            <h2 className='mt-5 mb-4 font-bold text-xl'>
+              Unique Words: {state.length}
+            </h2>
+
+            {state.map(([word, frequency]) => (
+              <p className='p-2 mb-2 flex shadow font-medium' key={word}>
+                {word} - {frequency}
+              </p>
+            ))}
+          </>
+        )}
       </div>
     </>
   );
