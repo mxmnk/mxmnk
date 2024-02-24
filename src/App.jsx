24751a61f447077b16e783e0aab95ecb81749f77
George Maximenko <30684663+mvximenko@users.noreply.github.com>
2024-02-24 15:53:04 +0300

    Implement parseEpub

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { parseEpub } from './utils/parseEpub';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const handleChange = async (e) => {
+    const file = e.target.files.item(0);
+
+    if (file) {
+      const parsed = await parseEpub(file);
+      console.log(parsed);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type='file' onChange={handleChange} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/utils/parseEpub.js
@@ -0,0 +1,139 @@
+import { unzip } from 'unzipit';
+
+const domParser = new DOMParser();
+const parseXml = (xml) => domParser.parseFromString(xml, 'application/xml');
+const parseHtml = (html) => domParser.parseFromString(html, 'text/html');
+
+const resolvePath = (str, root) => {
+  return str[0] === '/' ? str.slice(1) : root + str;
+};
+
+const getRoot = (opfPath) => {
+  let root = '';
+
+  if (opfPath.includes('/')) {
+    root = opfPath.replace(/\/([^/]+)\.opf/i, '');
+
+    if (!root.endsWith('/')) {
+      root += '/';
+    }
+
+    if (root.startsWith('/')) {
+      root = root.replace(/^\//, '');
+    }
+  }
+
+  return root;
+};
+
+const FORBIDDEN_TAGS = [
+  'head',
+  'link',
+  'style',
+  'script',
+  'textarea',
+  'input',
+  'svg',
+  'img',
+  'a',
+];
+
+const UNWRAP_TAGS = ['body', 'html', 'div', 'span'];
+
+const cleanHtml = async (text) => {
+  const doc = parseXml(text);
+
+  const forbiddenElements = [
+    ...doc.querySelectorAll(FORBIDDEN_TAGS.join(', ')),
+  ];
+
+  const unwrapElements = [
+    ...doc.querySelectorAll(
+      UNWRAP_TAGS.map((tag) => `${tag} > *:only-child`).join(', ')
+    ),
+  ];
+
+  for (const el of forbiddenElements) {
+    el.remove();
+  }
+
+  for (const el of unwrapElements) {
+    el.parentNode?.replaceChildren(...el.childNodes);
+  }
+
+  const content = (
+    doc.querySelector('body') ?? doc.documentElement
+  ).innerHTML.replace(/xmlns="(.*?)"/g, '');
+
+  return content;
+};
+
+const ALLOWED_ELEMENTS = new Set([
+  'P',
+  'H1',
+  'H2',
+  'H3',
+  'H4',
+  'H5',
+  'H6',
+  'OL',
+  'UL',
+  'ADDRESS',
+  'BLOCKQUOTE',
+  'DL',
+]);
+
+const getTextFromNodes = (element) => {
+  let text = '';
+
+  if (element.children.length && !ALLOWED_ELEMENTS.has(element.nodeName)) {
+    for (const child of element.children) {
+      text += getTextFromNodes(child);
+    }
+  } else {
+    text += element.textContent + ' ';
+  }
+
+  return text;
+};
+
+export const parseEpub = async (file) => {
+  const { entries } = await unzip(file);
+  const containerXml = parseXml(await entries['META-INF/container.xml'].text());
+
+  let opfPath = containerXml
+    .querySelector('rootfile')
+    ?.getAttribute('full-path');
+
+  if (opfPath == null) {
+    return;
+  }
+
+  const root = getRoot(opfPath);
+  const contentOpf = parseXml(
+    await entries[resolvePath('/' + opfPath, root)].text()
+  );
+
+  const manifest = contentOpf.querySelector('manifest');
+  const spine = [...contentOpf.querySelectorAll('spine > itemref')].map(
+    (item) => item.getAttribute('idref')
+  );
+
+  let htmlString = '';
+
+  for (const item of spine) {
+    const path = manifest
+      ?.querySelector(`item[id="${item}"]`)
+      ?.getAttribute('href');
+
+    if (path == null) {
+      return;
+    }
+
+    const html = await entries[resolvePath(path, root)].text();
+    const cleaned = await cleanHtml(html);
+    htmlString += cleaned + ' ';
+  }
+
+  return getTextFromNodes(parseHtml(htmlString).body);
+};
